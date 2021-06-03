a71d73db93e2db2771bbe332bcbf9e6fa009bbfb
DevDipin <69724747+DevDipin@users.noreply.github.com>
2021-06-03 11:13:27 -0400

    Fixes

--- src/main/java/net/helydev/com/commands/staff/CopyInventoryCommand.java
+++ src/main/java/net/helydev/com/commands/staff/CopyInventoryCommand.java
@@ -1,16 +1,9 @@
 package net.helydev.com.commands.staff;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import net.helydev.com.utils.commands.CommandArgs;
-import org.bukkit.Bukkit;
-import org.bukkit.command.Command;
-import org.bukkit.command.CommandExecutor;
-import org.bukkit.command.CommandSender;
-import org.bukkit.entity.Player;
-
 import net.md_5.bungee.api.ChatColor;
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
 
 /**
  * This command was added by DevDipin!
@@ -33,25 +26,14 @@ public class CopyInventoryCommand {
             return false;
         }
 
-        Player player = (Player) sender;
         Player target = Bukkit.getServer().getPlayerExact(args[0]);
         if (target == null) {
             sender.sendMessage(ChatColor.translateAlternateColorCodes('&', "&cPlayer with name or UUID with '" + args[0] + "' not found."));
         } else {
-            player.getInventory().setContents(target.getInventory().getContents());
-            player.getInventory().setArmorContents(target.getInventory().getArmorContents());
-            player.sendMessage(ChatColor.WHITE + "You have copied the inventory of " + ChatColor.AQUA + target.getName());
+            sender.getInventory().setContents(target.getInventory().getContents());
+            sender.getInventory().setArmorContents(target.getInventory().getArmorContents());
+            sender.sendMessage(ChatColor.WHITE + "You have copied the inventory of " + ChatColor.AQUA + target.getName());
         }
         return false;
-    }
-
-    public List<String> onTabComplete(CommandSender sender, Command command, String label, String[] args) {
-        List<String> results = new ArrayList<>();
-        for (Player target : Bukkit.getOnlinePlayers()) {
-            if (!results.contains(target.getName())) {
-                results.add(target.getName());
-            }
-        }
-        return results;
     }
 }
