3862169b147ee8776bd8bbe7e59608d1c82f9144
DevDipin <69724747+DevDipin@users.noreply.github.com>
2021-06-09 15:21:01 -0400

    Added Vouchers Config

--- src/main/java/net/helydev/com/listeners/VouchersListener.java
+++ src/main/java/net/helydev/com/listeners/VouchersListener.java
@@ -22,12 +22,12 @@ public class VouchersListener implements Listener {
     @EventHandler
     public void onInteractVouch(final PlayerInteractEvent event) {
         final Player player = event.getPlayer();
-        for (final String vouchItem : xCore.getPlugin().getConfig().getConfigurationSection("vouchers").getKeys(false)) {
-            final ItemStack vouchItemStackCustom = new ItemStack(Material.valueOf(xCore.getPlugin().getConfig().getString("vouchers." + vouchItem + ".item")), 1, (short) xCore.getPlugin().getConfig().getInt("vouchers." + vouchItem + ".item-data"));
+        for (final String vouchItem : xCore.getPlugin().getVoucherConfig().getConfiguration().getConfigurationSection("vouchers").getKeys(false)) {
+            final ItemStack vouchItemStackCustom = new ItemStack(Material.valueOf(xCore.getPlugin().getVoucherConfig().getConfiguration().getString("vouchers." + vouchItem + ".item")), 1, (short) xCore.getPlugin().getVoucherConfig().getConfiguration().getInt("vouchers." + vouchItem + ".item-data"));
             final ItemMeta vouchItemMetaCustom = vouchItemStackCustom.getItemMeta();
-            vouchItemMetaCustom.setDisplayName(xCore.getPlugin().getConfig().getString("vouchers." + vouchItem + ".name"));
+            vouchItemMetaCustom.setDisplayName(xCore.getPlugin().getVoucherConfig().getConfiguration().getString("vouchers." + vouchItem + ".name"));
             final List<String> lore = new ArrayList<>();
-            for (String string : xCore.getPlugin().getConfig().getStringList("vouchers." + vouchItem + ".lores")) {
+            for (String string : xCore.getPlugin().getVoucherConfig().getConfiguration().getStringList("vouchers." + vouchItem + ".lores")) {
                 string = string.replace("&", "§");
                 string = string.replace("%d_arrow%", "»");
                 lore.add(string);
@@ -37,11 +37,11 @@ public class VouchersListener implements Listener {
             if (player.getItemInHand().getItemMeta() == null || player.getItemInHand() == null || player.getItemInHand().getItemMeta().getDisplayName() == null || player.getItemInHand().getItemMeta().getLore() == null) {
                 return;
             }
-            if (!player.getPlayer().getItemInHand().getItemMeta().getDisplayName().equalsIgnoreCase(CC.translate(xCore.getPlugin().getConfig().getString("vouchers." + vouchItem + ".name")))) {
+            if (!player.getPlayer().getItemInHand().getItemMeta().getDisplayName().equalsIgnoreCase(CC.translate(xCore.getPlugin().getVoucherConfig().getConfiguration().getString("vouchers." + vouchItem + ".name")))) {
                 continue;
             }
-            final List<String> nopermcommands = xCore.getPlugin().getConfig().getStringList("vouchers." + vouchItem + ".permissions.commands");
-            final List<String> commands = xCore.getPlugin().getConfig().getStringList("vouchers." + vouchItem + ".commands");
+            final List<String> nopermcommands = xCore.getPlugin().getVoucherConfig().getConfiguration().getStringList("vouchers." + vouchItem + ".permissions.commands");
+            final List<String> commands = xCore.getPlugin().getVoucherConfig().getConfiguration().getStringList("vouchers." + vouchItem + ".commands");
             for (final String str : commands) {
                 Bukkit.dispatchCommand(Bukkit.getConsoleSender(), str.replace("%player%", player.getName()));
             }
@@ -50,31 +50,31 @@ public class VouchersListener implements Listener {
             } else {
                 player.setItemInHand(null);
             }
-            final List<String> message = xCore.getPlugin().getConfig().getStringList("vouchers." + vouchItem + ".opened");
+            final List<String> message = xCore.getPlugin().getVoucherConfig().getConfiguration().getStringList("vouchers." + vouchItem + ".opened");
             for (final String str2 : message) {
                 player.sendMessage(CC.translate(str2));
             }
 
             ///////////////////////////////////////// LISTENERS /////////////////////////////////////////////////////
             //Plays a sound when a player opens a voucher.
-            if (xCore.getPlugin().getConfig().getBoolean("vouchers." + vouchItem + ".sound.enabled")) {
-                player.playSound(player.getLocation(), Sound.valueOf(xCore.getPlugin().getConfig().getString("vouchers." + vouchItem + ".sound.sound-name").toUpperCase()), 1.0F, 1.0F);
+            if (xCore.getPlugin().getVoucherConfig().getConfiguration().getBoolean("vouchers." + vouchItem + ".sound.enabled")) {
+                player.playSound(player.getLocation(), Sound.valueOf(xCore.getPlugin().getVoucherConfig().getConfiguration().getString("vouchers." + vouchItem + ".sound.sound-name").toUpperCase()), 1.0F, 1.0F);
             }
             //Plays a particle effect around the player.
-            if (xCore.getPlugin().getConfig().getBoolean("vouchers." + vouchItem + ".particles.enabled")) {
+            if (xCore.getPlugin().getVoucherConfig().getConfiguration().getBoolean("vouchers." + vouchItem + ".particles.enabled")) {
                 player.playEffect(player.getLocation(), Effect.MOBSPAWNER_FLAMES, 0);
             }
             //Checks if it limits for people with specified permission.
-            if (xCore.getPlugin().getConfig().getBoolean("vouchers." + vouchItem + ".permissions.enabled")) {
-                if (!player.hasPermission(xCore.getPlugin().getConfig().getString("vouchers." + vouchItem + ".permissions.permission")))
-                    if (xCore.getPlugin().getConfig().getBoolean("vouchers." + vouchItem + ".permissions.send-message")) {
+            if (xCore.getPlugin().getVoucherConfig().getConfiguration().getBoolean("vouchers." + vouchItem + ".permissions.enabled")) {
+                if (!player.hasPermission(xCore.getPlugin().getVoucherConfig().getConfiguration().getString("vouchers." + vouchItem + ".permissions.permission")))
+                    if (xCore.getPlugin().getVoucherConfig().getConfiguration().getBoolean("vouchers." + vouchItem + ".permissions.send-message")) {
                         player.sendMessage(Color.translate(xCore.getPlugin().getMessageconfig().getConfiguration().getString("vouchers.no-permissions")));
                         return;
                     }
             }
             //Executes no permissions commands if you do not have permissions to use a voucher.
-            if (xCore.getPlugin().getConfig().getBoolean("vouchers." + vouchItem + ".permissions.commands-enabled")) {
-                if (!player.hasPermission(xCore.getPlugin().getConfig().getString("vouchers." + vouchItem + ".permissions.permission")))
+            if (xCore.getPlugin().getVoucherConfig().getConfiguration().getBoolean("vouchers." + vouchItem + ".permissions.commands-enabled")) {
+                if (!player.hasPermission(xCore.getPlugin().getVoucherConfig().getConfiguration().getString("vouchers." + vouchItem + ".permissions.permission")))
                     for (final String str : nopermcommands) {
                         Bukkit.dispatchCommand(Bukkit.getConsoleSender(), str.replace("%player%", player.getName()));
                     }
